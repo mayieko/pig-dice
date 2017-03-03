657ae7872646430cba2c87d26ad8ce0158807e6a
Mayieko <michaelmayieko@gmail.com>
2017-03-03 17:13:03 +0300

    navigation bar addd

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,3 @@
-var Player = function(turn) {
-  this.scoreTotal = 0;
-  this.currentScore = 0;
-  this.turn = turn;
-};
-
-Player.prototype.takeTurn = function() {
-  if (this.turn === true) {
-    this.turn = false;
-  }
-  else {
-    this.turn = true;
-  }
-};
-Player.prototype.roll = function() {
-  var rand = Math.floor(Math.random() * 6) + 1;
-  this.currentScore = rand;
-};
-
-Player.prototype.addPoints = function(rollPoints) {
-  this.scoreTotal += rollPoints;
-};
-
-var player1 = new Player(true);
-var player2 = new Player(false);
-
-var totalRoll;
-// User Interface Logic
 $(document).ready(function() {
   var playerOneName;
   var playerTwoName;
@@ -127,3 +99,33 @@ $(document).ready(function() {
  });
 
 }); // end ready
+
+
+var Player = function(turn) {
+  this.scoreTotal = 0;
+  this.currentScore = 0;
+  this.turn = turn;
+};
+
+Player.prototype.takeTurn = function() {
+  if (this.turn === true) {
+    this.turn = false;
+  }
+  else {
+    this.turn = true;
+  }
+};
+Player.prototype.roll = function() {
+  var rand = Math.floor(Math.random() * 6) + 1;
+  this.currentScore = rand;
+};
+
+Player.prototype.addPoints = function(rollPoints) {
+  this.scoreTotal += rollPoints;
+};
+
+var player1 = new Player(true);
+var player2 = new Player(false);
+
+var totalRoll;
+// User Interface Logic
